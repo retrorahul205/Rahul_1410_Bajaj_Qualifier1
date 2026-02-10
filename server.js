0d648d0c3c08b36baac8d1f0427df4fd6163c16b
retrorahul205 <167735447+retrorahul205@users.noreply.github.com>
2026-02-10 12:40:29 +0530

    Add root route and landing page

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname,'index.html'));
 });
 
 app.get('/health', (req, res) => {
