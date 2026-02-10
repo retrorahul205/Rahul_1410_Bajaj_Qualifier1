34b84a2dad45716102a55797e56da77b9c3775bb
retrorahul205 <167735447+retrorahul205@users.noreply.github.com>
2026-02-10 12:28:35 +0530

    Configure for Vercel deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const logicMap= require('./functions.js');
+const logicMap = require('./functions.js');
 
 app.use(express.json());
 
@@ -11,14 +11,13 @@ app.get('/health', (req, res) => {
    })
 });
 
-
 app.post('/bfhl', async (req, res) => {
     const rawkey = Object.keys(req.body)[0];
     const key = rawkey.toLowerCase();
     const value = req.body[rawkey];   
 
     const fn = logicMap[key];
-     if(!fn){
+    if(!fn){
         return res.json({
             is_success: false,
             official_email: "rahul1410.be23@chitkara.edu.in",
@@ -28,9 +27,9 @@ app.post('/bfhl', async (req, res) => {
 
     let result;
 
-    if(key==="ai"){
+    if(key === "ai"){
         result = await fn(value);   
-    }else{
+    } else {
         result = fn(value);
     }
 
@@ -41,6 +40,11 @@ app.post('/bfhl', async (req, res) => {
     });
 });
 
-app.listen(3000,() => {
-  console.log('Server running at http://localhost:3000/bfhl');
-});
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server running at http://localhost:${PORT}/bfhl`);
+    });
+}
+
+module.exports = app;
